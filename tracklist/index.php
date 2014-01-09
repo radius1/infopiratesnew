0e64c5f70a57ef990e3ad113f84014929cec757b
Dimmy <dimwes@hotmail.com>
2014-01-09 10:11:43 +0100

    header & container af

--- tracklist/index.php
+++ tracklist/index.php
@@ -18,6 +18,30 @@
                 </ul>
             </div>
         </div>
+        
+        <div id='slider'></div>
+        
+        <div id='mediaplayer'>
+            <audio id="controls" controls="controls">
+              <source src="music/johnny_i_hardly_knew_ya.mp3" type="audio/mp3">
+                Your browser does not support this audio format.
+            </audio>
+            
+            <p>"Do what you want, 'cause a pirate is free, You are a pirate!"</p>
+        </div>
+        
+        <div class="container">
+                    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. 
+                    </p>
+                <figure>
+                    <img src="img/pirates_6.jpg">
+                </figure>
+                
+        </div>
+        
+        <div id="footer-wrapper">
+            <p>&#169; Copyright Dimmy Marcelissen Mediadeveloper MED2A Radius College </p>
+        </div>
     </div>        
 </body>
 </html>
