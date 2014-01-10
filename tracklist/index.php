91964eca98e3702dc2185ee7dc39f0846887c5f3
Dimmy <dimwes@hotmail.com>
2014-01-10 16:21:44 +0100

    Tracklist toegevoegd

--- tracklist/index.php
+++ tracklist/index.php
@@ -22,26 +22,84 @@
         <div id='slider'></div>
         
         <div id='mediaplayer'>
-            <audio id="controls" controls="controls">
-              <source src="music/johnny_i_hardly_knew_ya.mp3" type="audio/mp3">
-                Your browser does not support this audio format.
+            <audio src="http://www.archive.org/download/bolero_69/Bolero.mp3#sthash.udlQoyUQ.dpuf" id="audio" preload="auto" tabindex="0" controls="">
+                Please use a modern browser, like come on!
             </audio>
-            
-            <p>"Do what you want, 'cause a pirate is free, You are a pirate!"</p>
         </div>
         
         <div class="container">
                     <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. 
                     </p>
-                <figure>
-                    <img src="img/pirates_6.jpg">
-                </figure>
+                    <ul id="playlist">
+                        <li class="active">
+                            <a href="https://ia600201.us.archive.org/5/items/pirate/PiratesLifeForMe.mp3">
+                                Pirates Life For Me
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://ia700308.us.archive.org/23/items/YouAreAPirate/YouAreAPirate.mp3">
+                                You Are A Pirate
+                            </a>    
+                        </li>
+                        <li>
+                        <a href="http://davidrovics.com/kids/music/05-The-Pirate-Song.mp3">
+                            The Pirate Song
+                        </a>
+                        </li>
+                        <li class="active">
+                            <a href="music/black_flag.mp3">
+                                Black Flag
+                            </a>
+                        </li>
+                        <li>
+                            <a href="music/irisch_tavern.mp3">
+                                Irisch Tavern    
+                            </a>
+                        </li>
+                        <li>
+                        <a href="music/the_drunk_scotsman.mp3">
+                            The Drunk Scotsman
+                        </a>
+                        </li>
+                        <li>
+                            <a href="music/bottle_of_rum.mp3">
+                                Bottle Of Rum
+                            </a>
+                        </li>
+                        <li>
+                            <a href="music/pirate_tavern.mp3">
+                            Pirate Tavern
+                            </a>
+                        </li>
+                        <li>
+                        <a href="music/epic_music.mp3">
+                            Epic Pirate
+                        </a>
+                        </li>
+                        <li class="active">
+                            <a href="music/johnny_i_hardly_knew_ye.mp3">
+                                Johnny I Hardly Knew Ya
+                            </a>
+                        </li>
+                        <li>
+                            <a href="music/escaping_the_kraken.mp3">
+                                Escaping The Kraken
+                            </a>
+                        </li>                        
+                    </ul>    
+                
                 
         </div>
-        
         <div id="footer-wrapper">
             <p>&#169; Copyright Dimmy Marcelissen Mediadeveloper MED2A Radius College </p>
         </div>
-    </div>        
+        
+        
+    </div>
+    
+    
+    <script src="http://code.jquery.com/jquery-2.0.3.min.js"></script>
+    <script src="script.js"></script>    
+        
 </body>
 </html>
